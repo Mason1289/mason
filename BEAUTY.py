7a28adadde9df86640d5724a732d74c1ef6fb8fb
Mason1289 <80187257+Mason1289@users.noreply.github.com>
2021-04-01 08:24:32 +0500

    Update BEAUTY.py

--- BEAUTY.py
+++ BEAUTY.py
@@ -1,6 +1,24 @@
-#!/bin/sh/
+#!/usr/bin/mason test
+import sys
+import time
+import socket
+import struct
+import threading
+from random import randint
+from optparse import OptionParser
+from pinject import IP, UDP
 
-# Author : MASON
-echo "HELLO BRO!"
-read PERSON
-echo "MADE BY, $PERSON"
+USAGE = '''
+%prog target.com [options]        # DDoS
+%prog benchmark [options]         # Calculate AMPLIFICATION factor
+'''
+
+LOGO = r'''
+	   _____           __    __              
+	  / ___/____ _____/ /___/ /___ _____ ___ 
+	  \__ \/ __ `/ __  / __  / __ `/ __ `__ \
+	 ___/ / /_/ / /_/ / /_/ / /_/ / / / / / /
+	/____/\__,_/\__,_/\__,_/\__,_/_/ /_/ /_/ 
+	https://github.com/OffensivePython/Saddam
+	   https://twitter.com/OffensivePython
+'''
